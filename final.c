4a7e109251775d267f4461974c45ecc4c6f8650f
Christopher Jordan <christopher.james.jordan@gmail.com>
2014-11-28 20:07:22 -0700

    working on panels again

--- final.c
+++ final.c
@@ -20,7 +20,7 @@ double asp=1;     //  Aspect ratio
 double dim=40.0;   //  Size of world
 
 // Texture array
-unsigned int texture[14]; // Texture names
+unsigned int texture[15]; // Texture names
 GLuint cockpitTex;
 
 // Light values
@@ -651,33 +651,31 @@ static void xWing(double x, double y, double z,
    glTexCoord2f(1,1); glVertex3d(2,+2.5,+7);
    glEnd();
 
-   glBindTexture(GL_TEXTURE_2D,texture[8]);
+   glBindTexture(GL_TEXTURE_2D,texture[14]);
    glBegin(GL_POLYGON);
    glColor3f(0.75,0.75,0.75);
-   normal(2,2.5,7, 3,4,-3, 4,2,-3);
-   glTexCoord2f(-1,1); glVertex3d(3,+4,-3);
-   glTexCoord2f(-1,1); glVertex3d(4,+2,-3);
+   normal(2,2.5,7, 3,5,-2, 3,5,0);
+   glTexCoord2f(1,1); glVertex3d(3,5,0);
+   glTexCoord2f(1,0); glVertex3d(3,5,-2);
+   glTexCoord2f(0,0); glVertex3d(2,+2.5,+7);
+   glEnd();
+
+   glBindTexture(GL_TEXTURE_2D,texture[14]);
+   glBegin(GL_POLYGON);
+   glColor3f(0.75,0.75,0.75);
+   normal(2,2.5,7, 3.8,2,-2, 3,5,-2);
+   glTexCoord2f(-1,1); glVertex3d(3,5,-2);
+   glTexCoord2f(-1,1); glVertex3d(3.8,2,-2);
    glTexCoord2f(1,1); glVertex3d(2,+2.5,+7);
    glEnd();
 
-   glBindTexture(GL_TEXTURE_2D,texture[8]);
+   glBindTexture(GL_TEXTURE_2D,texture[14]);
    glBegin(GL_POLYGON);
    glColor3f(0.75,0.75,0.75);
-   normal(2,2.5,7, 3,5,0, 3,4,-3);
+   normal(2,2.5,7, 4.7,0.25,-2, 3.8,2,-2);
+   glTexCoord2f(-1,1); glVertex3d(3.8,2,-2);
+   glTexCoord2f(-1,1); glVertex3d(4.7,0.25,-2);
    glTexCoord2f(1,1); glVertex3d(2,+2.5,+7);
-   glTexCoord2f(1,0); glVertex3d(3,+5,+0);
-   glTexCoord2f(1,0); glVertex3d(3,+5,-2);
-   glTexCoord2f(-1,1); glVertex3d(3,+4,-3);
-   glEnd();
-
-   glBindTexture(GL_TEXTURE_2D,texture[8]);
-   glBegin(GL_POLYGON);
-   glColor3f(0.75,0.75,0.75);
-   normal(2,2.5,7, 3,4,-3, 3,5,0);
-   glTexCoord2f(1,1); glVertex3d(2,+2.5,+7);
-   glTexCoord2f(1,0); glVertex3d(3,+5,+0);
-   glTexCoord2f(1,0); glVertex3d(3,+5,-2);
-   glTexCoord2f(-1,1); glVertex3d(3,+4,-3);
    glEnd();
 
    // Cockpit window panel
@@ -1779,6 +1777,7 @@ int main(int argc,char* argv[])
    texture[11] = LoadTexBMP("Vinyl_Gray_2.bmp");
    texture[12] = LoadTexBMP("red_material.bmp");
    texture[13] = LoadTexBMP("Vinyl_Gray_1.bmp");
+   texture[14] = LoadTexBMP("Carpet_Gray.bmp");
 	
 	cockpitTex = LoadTexBMP("TIECockpit.bmp");
 
